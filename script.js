edec5e1e89ea60315f545d5b6db1af08080cdad7
patel rushi <pate0672@algonquinlive.com>
2020-06-04 03:03:38 -0400

    stage one

---
+++ script.js
@@ -0,0 +1,8 @@
+function drawBoxes() {
+	var html = `<div class="red-box"></div>`;
+	var body = (document.getElementsByTagName('body')[0].innerHTML = html);
+	console.log(body);
+}
+import React, { Component } from 'react';
+
+then;
